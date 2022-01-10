4e00b2b038a9aa2a2f10c0bb8b6d2c7a71c6f1a6
clappingmin <clappingmin@naver.com>
2022-01-10 21:10:19 +0900

    메인화면

--- app.py
+++ app.py
@@ -10,16 +10,29 @@ db = client.dbsparta
 SECRET_KEY = 'YOUTUVERSE'
 
 import jwt
-
 import datetime
-
 import hashlib
 
-# router
+# HTML 화면 보여주기
 @app.route('/')
-def home_page():
-    nickname = 'test'
-    return render_template('index.html', nickname = nickname)
+def home():
+    return render_template('index.html')
+
+
+# 유튜버 전체 목록 가져오기
+@app.route('/api/youtuber/all', methods=['GET'])
+def show_all_youtuber():
+    youtuber = list(db.youtuber.find({}, {'_id': False}).sort("name"))
+
+    return jsonify({'youtubers': youtuber})
+
+
+# 유튜버 좋아요 상위 3명 목록 가져오기
+@app.route('/api/youtuber/top', methods=['GET'])
+def show_top3_youtuber():
+    youtuber = list(db.youtuber.find({}, {'_id': False}).limit(3).sort("likes", -1))
+
+    return jsonify({'youtubers': youtuber})
 
 @app.route('/login')
 def login_page():

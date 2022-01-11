744f0d545996aee5d4412621637913ee43b2cd95
clappingmin <clappingmin@naver.com>
2022-01-11 18:25:07 +0900

    네비게이션바에 닉네임, 카드 디자인 수정

--- app.py
+++ app.py
@@ -116,5 +116,19 @@ def show_all_youtuber():
     youtuber = list(db.youtuber.find({}, {'_id': False}).sort("name"))
     return jsonify({'youtubers': youtuber})
 
+# 유튜버 상세페이지로 데이터 전달
+@app.route('/api/youtuber/<id>')
+def show_want_youtuber(id):
+    # id, name, photoURL, tags, likes
+    youtuber = db.youtuber.find_one({'id':id})
+    name = youtuber['name']
+    photoURL = youtuber['photoURL']
+    tags = youtuber['tags']
+    likes = youtuber['likes']
+
+    return render_template('detail.html', id = id, name = name, photoURL = photoURL, tags = tags, likes = likes)
+
+
+
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)

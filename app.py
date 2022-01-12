3e0c0fb148d42c4764159438a8deea93b375a496
Hyeon-Gwang <wkdgusrhkd@gmail.com>
2022-01-12 16:10:58 +0900

    호빈님 작업 내용 합침

--- app.py
+++ app.py
@@ -7,6 +7,11 @@ from pymongo import MongoClient
 client = MongoClient('localhost', 27017)
 db = client.dbsparta
 
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from time import sleep
+
 SECRET_KEY = 'YOUTUVERSE'
 
 import jwt
@@ -44,6 +49,11 @@ def login_page():
 def pw_find_page():
     return render_template('login_pw.html')
 
+# URL 저장
+@app.route('/search')
+def search():
+    return render_template('search.html')
+
 # 유튜버 상세페이지로 데이터 전달
 @app.route('/youtuber/<id>')
 def show_want_youtuber(id):
@@ -63,6 +73,12 @@ def show_want_youtuber(id):
         return redirect(url_for('login_page', msg = '로그인 시간이 만료되었습니다.'))
     except jwt.exceptions.DecodeError:
         return render_template('detail.html', user = None, id = id, name = name, photoURL = photoURL, likes = likes, url = url, videoSrc = videoSrc)
+
+# 상세페이지
+# @app.route('/detail/<keyword>')
+# def detail(keyword):
+#     youtubers = list(db.youtube.find({}, {'_id': False}))
+#     return render_template('detail.html', youtubers=youtubers, keyword=keyword)
 
 # APIs
 # 회원가입
@@ -128,6 +144,55 @@ def find_password():
     db.users.update_one({ 'user_id': user_id }, { '$set': { 'password': hashed_password } })
 
     return jsonify({ 'msg': '비밀번호를 재설정하였습니다.' })
+
+# 좋아요 버튼 누르면 1개 증가하는 API
+@app.route('/api/like', methods=['POST'])
+def like_youtube():
+    name_receive = request.form['name_give']
+    a_like = db.youtube.find_one({'name': name_receive})
+    current_like = a_like['like']
+    new_like = current_like + 1
+    db.youtube.update_one({'name': name_receive}, {'$set': {'like': new_like}})
+    return jsonify({'msg': '좋아요!'})
+
+# 유튜버 크롤링
+@app.route('/api/search', methods=['POST'])
+def signUp():
+
+    # 드라이버를 실행합니다.
+    url = request.form['url_give']
+    # headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+    # data = requests.get(url, headers=headers)
+
+    s = Service('C:/chromedriver.exe')
+    driver = webdriver.Chrome(service=s)
+    driver.get(url)  # 드라이버에 해당 url의 웹페이지를 띄웁니다.
+    sleep(1)  # 페이지가 로딩되는 동안 5초 간 기다립니다.
+
+    req = driver.page_source  # html 정보를 가져옵니다.
+    driver.quit()  # 정보를 가져왔으므로 드라이버는 꺼줍니다.
+
+    # soup = BeautifulSoup(data.text, 'html.parser')
+    soup = BeautifulSoup(req, 'html.parser')  # 가져온 정보를 beautifulsoup으로 파싱해줍니다.
+
+    name = soup.select_one('meta[property="og:title"]')['content']
+    desc = soup.select_one('meta[property="og:description"]')['content']
+    subscribers = soup.select_one('yt-formatted-string#subscriber-count').text
+    photoURL = soup.select_one('meta[property="og:image"]')['content']
+    thumbnail_video = 'https://www.youtube.com' + soup.select_one('div#items a#thumbnail', href=True)['href']
+
+    doc = {
+        'like': 0,
+        'name': name,
+        'desc': desc,
+        'photoURL': photoURL,
+        'thumbnail_video': thumbnail_video,
+        'subscribers': subscribers,
+    }
+
+    db.youtube.insert_one(doc)
+
+    return jsonify({'result': 'success', 'msg': '업로드 완료!'})
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
